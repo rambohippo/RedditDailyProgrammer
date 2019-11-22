bb6a0590255fed31f6f053f099a616d682a11969
rambohippo <jcmcgarry6@gmail.com>
2019-11-22 09:31:40 -0800

    Add more methods of solving the challenge
    Add more methods of finding the max integer value in a map. To be used as future reference for java 8+ comparators.

--- src/main/java/dailyprogrammer/Challenge381Yahtzee.java
+++ src/main/java/dailyprogrammer/Challenge381Yahtzee.java
@@ -21,9 +21,64 @@ public class Challenge381Yahtzee {
     }
 
 
+    public static int yahtzee_upper_integer_compare(int[] diceRolls) {
+        return findMaxWithIntegerCompare(diceRollsToMap(diceRolls));
+    }
+
+
+    public static int yahtzee_upper_stream(int[] diceRolls) {
+        return findMaxWithStream(diceRollsToMap(diceRolls));
+    }
+
+
+    public static int yahtzee_upper_comparator(int[] diceRolls) {
+        return findMaxComparator(diceRollsToMap(diceRolls));
+    }
+
+
+    public static int yahtzee_upper_comparing_int(int[] diceRolls) {
+        return findMaxComparingInt(diceRollsToMap(diceRolls));
+    }
+
+
     private static void incrementMapValue(Map<Integer, Integer> map, Integer key) {
         if (map.computeIfPresent(key, (k, v) -> v.intValue() + k.intValue()) == null) {
             map.put(key, key);
         }
+    }
+
+
+    private static Map<Integer, Integer> diceRollsToMap(int[] diceRolls) {
+        HashMap<Integer, Integer> diceMap = new HashMap<>();
+
+        for (int i = 0; i < diceRolls.length; i++) {
+            incrementMapValue(diceMap, diceRolls[i]);
+        }
+
+        return diceMap;
+    }
+
+    
+    private static Integer findMaxWithIntegerCompare(Map<Integer, Integer> diceRolls) {
+        return diceRolls.entrySet().stream().max((entry1, entry2) ->
+            Integer.compare(entry1.getValue(), entry2.getValue())).get().getValue();
+    }
+
+
+    private static Integer findMaxWithStream(Map<Integer, Integer> map) {
+        return map.entrySet().stream().max((entry1, entry2) ->
+            entry1.getValue() > entry2.getValue() ? 1 : -1).get().getValue();
+    }
+
+
+    private static Integer findMaxComparator(Map<Integer, Integer> map) {
+        return Collections.max(map.entrySet(), (entry1, entry2) ->
+            (entry1.getValue() - entry2.getValue())).getValue();
+    }
+
+
+    private static Integer findMaxComparingInt(Map<Integer, Integer> map) {
+        return Collections.max(map.entrySet(),
+            Comparator.comparingInt(Map.Entry::getValue)).getValue();
     }
 }
--- src/test/java/dailyprogrammer/Challenge381YahtzeeTest.java
+++ src/test/java/dailyprogrammer/Challenge381YahtzeeTest.java
@@ -6,34 +6,37 @@ import org.junit.jupiter.api.Test;
 public class Challenge381YahtzeeTest {
     
     @Test
-    public void test1() {
+    public void testBasic() {
         assertEquals(10, Challenge381Yahtzee.yahtzee_upper(new int[] {2, 3, 5, 5, 6}));
     }
 
     @Test
-    public void test2() {
-        assertEquals(4, Challenge381Yahtzee.yahtzee_upper(new int[] {1, 1, 1, 1, 3}));
+    public void testIntegerCompare() {
+        assertEquals(4, Challenge381Yahtzee.yahtzee_upper_integer_compare(new int[] {1, 1, 1, 1, 3}));
     }
 
     @Test
-    public void test3() {
-        assertEquals(6, Challenge381Yahtzee.yahtzee_upper(new int[] {1, 1, 1, 3, 3}));
+    public void testStream() {
+        assertEquals(6, Challenge381Yahtzee.yahtzee_upper_stream(new int[] {1, 1, 1, 3, 3}));
     }
 
     @Test
-    public void test4() {
-        assertEquals(5, Challenge381Yahtzee.yahtzee_upper(new int[] {1, 2, 3, 4, 5}));
+    public void testComparator() {
+        assertEquals(5, Challenge381Yahtzee.yahtzee_upper_comparator(new int[] {1, 2, 3, 4, 5}));
     }
 
     @Test
-    public void test5() {
-        assertEquals(30, Challenge381Yahtzee.yahtzee_upper(new int[] {6, 6, 6, 6, 6}));
+    public void testComparingInt() {
+        assertEquals(30, Challenge381Yahtzee.yahtzee_upper_comparing_int(new int[] {6, 6, 6, 6, 6}));
     }
 
     @Test
     public void testLargeNumbers() {
-        assertEquals(123456, Challenge381Yahtzee.yahtzee_upper(new int[] {1654,
-            1654, 50995, 30864, 1654, 50995, 22747, 1654, 1654, 1654, 1654,
-            1654, 30864, 4868, 1654, 4868, 1654, 30864, 4868, 30864}));
+        int[] testArray = new int[] { 1654, 1654, 50995, 30864, 1654, 50995, 22747, 1654, 1654, 1654,
+            1654, 1654, 30864, 4868, 1654, 4868, 1654, 30864, 4868, 30864 };
+        assertEquals(123456, Challenge381Yahtzee.yahtzee_upper_integer_compare(testArray));
+        assertEquals(123456, Challenge381Yahtzee.yahtzee_upper_stream(testArray));
+        assertEquals(123456, Challenge381Yahtzee.yahtzee_upper_comparator(testArray));
+        assertEquals(123456, Challenge381Yahtzee.yahtzee_upper_comparing_int(testArray));
     }
 }
