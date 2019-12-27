798e513a7169d03bdfe149716a3159f54c9eb4fd
rambohippo <jcmcgarry6@gmail.com>
2019-12-27 14:50:25 -0800

    [E377] Add code and tests for fit2

--- src/main/java/dailyprogrammer/E377AxisAlignedPacking.java
+++ src/main/java/dailyprogrammer/E377AxisAlignedPacking.java
@@ -5,4 +5,9 @@ public class E377AxisAlignedPacking {
     public static int fit1(int crateX, int crateY, int boxX, int boxY) {
         return (crateX / boxX) * (crateY / boxY);
     }
+
+
+    public static int fit2(int crateX, int crateY, int boxX, int boxY) {
+        return Math.max(fit1(crateX, crateY, boxX, boxY), fit1(crateX, crateY, boxY, boxX));
+    }
 }
--- src/test/java/dailyprogrammer/E377AxisAlignedPackingTest.java
+++ src/test/java/dailyprogrammer/E377AxisAlignedPackingTest.java
@@ -29,4 +29,34 @@ public class E377AxisAlignedPackingTest {
     public void testFit1_5() {
         assertEquals(0, E377AxisAlignedPacking.fit1(5, 100, 6, 1));
     }
+
+    @Test
+    public void testFit2_1() {
+        assertEquals(15, E377AxisAlignedPacking.fit2(25, 18, 6, 5));
+    }
+
+    @Test
+    public void testFit2_2() {
+        assertEquals(12, E377AxisAlignedPacking.fit2(12, 34, 5, 6));
+    }
+
+    @Test
+    public void testFit2_3() {
+        assertEquals(5676, E377AxisAlignedPacking.fit2(12345, 678910, 1112, 1314));
+    }
+
+    @Test
+    public void testFit2_4() {
+        assertEquals(2, E377AxisAlignedPacking.fit2(5, 5, 3, 2));
+    }
+
+    @Test
+    public void testFit2_5() {
+        assertEquals(80, E377AxisAlignedPacking.fit2(5, 100, 6, 1));
+    }
+
+    @Test
+    public void testFit2_6() {
+        assertEquals(0, E377AxisAlignedPacking.fit2(5, 5, 6, 1));
+    }
 }
